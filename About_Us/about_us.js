b2840ac1136ce9fcce8bacbaa38de5bed68706c8
Artemis <arsitur4@gmail.com>
2023-04-08 12:08:07 +0300

    Adaugat SVGs, Adaugat si font, sters nenecesar, schimbat numele

--- About_Us/about_us.js
+++ About_Us/about_us.js
@@ -1,34 +1,34 @@
 // Progress Bar
 
-const progressBar = document.querySelector('.progress-bar');
-const progressBarwithBonus = document.querySelector('#header-top-bar-bonus-progress');
-window.addEventListener('scroll', () => {
+// const progressBar = document.querySelector('.progress-bar');
+// const progressBarwithBonus = document.querySelector('#header-top-bar-bonus-progress');
+// window.addEventListener('scroll', () => {
   
 
-  const {scrollTop, scrollHeight} = document.documentElement;
+//   const {scrollTop, scrollHeight} = document.documentElement;
 
-  const scrollPercent = scrollTop / (scrollHeight - window.innerHeight) * 100;
+//   const scrollPercent = scrollTop / (scrollHeight - window.innerHeight) * 100;
 
-  if(scrollPercent >= 99)
-  {
-    progressBar.classList.add('progress-bar-anim');
-  }
-  else
-  {
-    progressBar.classList.remove('progress-bar-anim');
-  }
-})
+//   if(scrollPercent >= 99)
+//   {
+//     progressBar.classList.add('progress-bar-anim');
+//   }
+//   else
+//   {
+//     progressBar.classList.remove('progress-bar-anim');
+//   }
+// })
 
-function updateProgressBar() 
-{
-	const {scrollTop, scrollHeight} = document.documentElement;
+// function updateProgressBar() 
+// {
+// 	const {scrollTop, scrollHeight} = document.documentElement;
 
-	const scrollPercent = scrollTop / (scrollHeight - window.innerHeight) * 100 + '%';
+// 	const scrollPercent = scrollTop / (scrollHeight - window.innerHeight) * 100 + '%';
 
-	progressBar.style.setProperty('--progress', scrollPercent);  
-}
+// 	progressBar.style.setProperty('--progress', scrollPercent);  
+// }
 
-document.addEventListener('scroll', updateProgressBar);
+// document.addEventListener('scroll', updateProgressBar);
   
 
 
