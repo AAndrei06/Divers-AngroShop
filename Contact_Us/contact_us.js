0d68efc7c180be715640f93da358483f3c26f484
Artemis <arsitur4@gmail.com>
2023-04-20 22:13:40 +0300

    Adaugat FAQ

--- Contact_Us/contact_us.js
+++ Contact_Us/contact_us.js
@@ -5,7 +5,7 @@ let formButtonIcon = document.querySelector('.form-button-icon');
 let formButtonText = document.querySelector('.form-button-text');
 
 
-
+// Animatie
 submitButton.addEventListener('click', (e) =>
 {
 	e.preventDefault();
@@ -20,7 +20,7 @@ submitButton.addEventListener('click', (e) =>
 
 
 
-
+// Sa dispara textu din inputuri cand apesi pe un input :)
 let formInputs = document.querySelectorAll('.form-input');
 let formDescription = document.querySelector('.form-input-description');
 
@@ -32,7 +32,11 @@ formInputs.forEach(input =>
 	})
 	input.addEventListener('blur', () =>
 	{
-		input.previousElementSibling.style.display = 'flex';
+		if(input.value.trim() == '')
+		{
+			input.previousElementSibling.style.display = 'flex';
+		}
+		
 	})
 })
 
@@ -42,5 +46,60 @@ formDescription.addEventListener('focus', () =>
 })
 formDescription.addEventListener('blur', () =>
 {
-	formDescription.previousElementSibling.style.display = 'flex';
+	if(formDescription.value.trim() == '')
+	{
+		formDescription.previousElementSibling.style.display = 'flex';
+	}
 })
+
+
+// FAQ
+
+const faqs = document.querySelectorAll('.faq');
+const faqIcons = document.querySelectorAll('.faq-icon');
+const faqAnswers = document.querySelectorAll('.faq-answer');
+
+faqs.forEach(faq =>
+{
+	faq.addEventListener('click', () =>
+	{
+		remove_currentFAQ();
+		faq.children[0].children[1].classList.add('current-faq-icon');
+
+		faq.children[1].classList.add('expand-answer');
+
+	})
+})
+
+function remove_currentFAQ()
+{
+	faqAnswers.forEach(answer =>
+	{
+		if(answer.classList.contains('expand-answer'))
+		{
+			answer.classList.remove('expand-answer');
+		}
+	})
+	faqIcons.forEach(icon =>
+	{
+		if(icon.classList.contains('current-faq-icon'))
+		{
+			icon.classList.remove('current-faq-icon');
+		}
+	})
+}
+
+
+// RANDOM CURRENT FAQ 
+
+choose_currentFAQ();
+
+function choose_currentFAQ()
+{
+	let totalFAQ = faqs.length;
+	let randomNum = Math.floor(Math.random() * totalFAQ);
+	
+
+	faqAnswers[randomNum].classList.add('expand-answer');
+	faqIcons[randomNum].classList.add('current-faq-icon');
+}
