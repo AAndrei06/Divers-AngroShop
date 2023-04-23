ec9f76fdcc7031941572c5d65d501265bbe65ef0
Artemis <arsitur4@gmail.com>
2023-04-23 16:47:55 +0300

    Terminat cu tot legat de slideshow

--- About_Us/about_us.js
+++ About_Us/about_us.js
@@ -1,10 +1,15 @@
-let slideShowContainer = document.querySelector('.slide-show-div');
-let slideSection = document.querySelector('#slide-show-section');
-let blackOverlay = document.querySelector('.slideshow-overlay-black');
-let grayOverlay = document.querySelector('.slideshow-overlay-gray');
+const slideShowContainer = document.querySelector('.slide-show-div');
+const slideSection = document.querySelector('#slide-show-section');
+const blackOverlay = document.querySelector('.slideshow-overlay-black');
+const grayOverlay = document.querySelector('.slideshow-overlay-gray');
+const slideshowSlidesDiv = document.querySelector('.slideshow-slides');
+const slideshowIndicatorsDiv = document.querySelector('.slideshow-indicators');
+const slideshowIndicatorAnim = document.querySelector('.slideshow-indicator-anim');
+const generalIndicator = document.querySelector('.slideshow-indicator');
+const allIndicators = document.querySelectorAll('.slideshow-indicator');
+const slideshowRadios = document.querySelectorAll('.slideshow-radio');
 
-
-
+	
 const slideSectionstyles = window.getComputedStyle(slideSection);
 let MAX_TOP = parseInt(slideSectionstyles.getPropertyValue('height'));
 let MAX_DISTANCE = slideSection.offsetTop + MAX_TOP;
@@ -24,15 +29,32 @@ let currentHeight = slideShowContainer.offsetHeight;
 let NEEDED_HEIGHT = MAX_HEIGHT - currentHeight;
 let heightPerFrame = NEEDED_HEIGHT / TOTAL_PX;
 
-let MAX_OPACITY = 0;
+const MAX_OPACITY = 0;
 let currentOpacity = 0.35;
 let opacityperFrame = currentOpacity / TOTAL_PX;
 
 
 
+
+
+preInstall_Slideshow();
+change_slideShow();
+
+
 window.addEventListener('scroll', () =>
 {
+	change_slideShow();
+})
+add_EventListenerIndicator();
+add_slideshowGrab();
 
+
+
+
+
+
+function change_slideShow()
+{
 	let {scrollTop, scrollHeight} = document.documentElement;
 	scrollTop = scrollTop / 3.5;	
 
@@ -51,23 +73,17 @@ window.addEventListener('scroll', () =>
 		if(TOP >= TOTAL_PX)
 		{
 			slideShowContainer.style.top = `${TOTAL_PX}px`;
-			slideShowContainer.style.filter = 'drop-shadow(0px 85px 24px rgba(0, 0, 0, 0.01)) drop-shadow(0px 54px 22px rgba(0, 0, 0, 0.04)) drop-shadow(0px 31px 18px rgba(0, 0, 0, 0.15)) drop-shadow(0px 14px 14px rgba(0, 0, 0, 0.26)) drop-shadow(0px 3px 7px rgba(0, 0, 0, 0.29)) drop-shadow(0px 0px 0px rgba(0, 0, 0, 0.3))';
 		}
-		else
-		{
-			slideShowContainer.style.filter = 'none';
-		}
+
 		if(TOP >= 0 && TOP < TOTAL_PX)
 		{
 			slideShowContainer.style.top = `${TOP}px`;
 		}
-
 		if(currentWidth <= MAX_WIDTH)
 		{
-			
 			slideShowContainer.style.width = `${currentWidth}px`;
+			slideshowIndicatorsDiv.style.width = `${currentWidth}px`;
 		}	
-
 		if(currentHeight <= MAX_HEIGHT)
 		{
 			slideShowContainer.style.height = `${currentHeight}px`;
@@ -78,11 +94,141 @@ window.addEventListener('scroll', () =>
 			grayOverlay.style.opacity = `${currentOpacity}`;
 		}
 
-		
-		
+		slideshowIndicatorAnim.style.width = `${generalIndicator.offsetWidth}px`;
+		indicatorMoveAnim();
+
+
+
 	}
+}
+
+function preInstall_Slideshow()
+{
+	let {scrollTop} = document.documentElement;
+
+	if(scrollTop >= slideShowContainer.offsetTop + slideSection.offsetTop)
+	{
+		slideShowContainer.style.top = `${TOTAL_PX}px`;
+		blackOverlay.style.opacity = 0;
+		grayOverlay.style.opacity = 0;
+		slideShowContainer.style.width = `${MAX_WIDTH}px`;
+		slideShowContainer.style.height = `${MAX_HEIGHT}px`;
+		slideshowIndicatorsDiv.style.width = `${MAX_WIDTH}px`;
+	}
+}
+
+function add_EventListenerIndicator()
+{
+	allIndicators.forEach(indicator =>
+	{
+		indicator.addEventListener('click', () =>
+		{
+			setTimeout(() =>
+			{
+				indicatorMoveAnim();
+			}, 20)
+			
+		})
+	})
+}
+
+function indicatorMoveAnim()
+{	
+	let radioIndex = get_indexCheckedRadio();
+	slideshowIndicatorAnim.style.left = `${allIndicators[radioIndex].offsetLeft}px`;
+}
+
+function get_indexCheckedRadio()
+{
+	let index = 0;
+	let actualIndex;
+
+	slideshowRadios.forEach(radio =>
+	{
+		if(!radio.checked)
+		{
+			index += 1;
+		}
+		else
+		{
+			actualIndex = index;
+		}
+	})
+
+	return actualIndex;
+}
+
+function get_checkedRadio()
+{
+	let tempRadio;
+
+	slideshowRadios.forEach(radio =>
+	{
+		if(radio.checked)
+		{
+			tempRadio = radio;
+		}
+	})
+
+	return tempRadio;
+}
+
+function add_slideshowGrab()
+{
+    let slidesStartX = 0;
+    let slidesScrollLeft = 0;
+
+    let firstMouse = 0;
+    let secondMouse = 0;
+
+    let mouseDowned = false;
+    let mouseUpped = false;
+
+    slideshowSlidesDiv.addEventListener('mousedown', (event) => 
+    {
+      slideshowSlidesDiv.style.cursor = 'grab';
+      slidesStartX = event.pageX - slideshowSlidesDiv.offsetLeft;
+      slidesScrollLeft = slideshowSlidesDiv.scrollLeft;
+      mouseDowned = true;
+      firstMouse = slidesStartX;
+    });
+
+    document.addEventListener('mouseup', () => 
+    {
+      slideshowSlidesDiv.style.cursor = 'initial';
+      slidesStartX = event.pageX - slideshowSlidesDiv.offsetLeft;
+      secondMouse = slidesStartX;
+      mouseUpped = true;
+
+      if(mouseDowned == true && mouseUpped == true)
+      {
+        let direction = firstMouse - secondMouse;
+        mouseDowned = false;
+        mouseUpped = false;
+        firstMouse = 0;
+        secondMouse = 0;
+
+        if(direction < 0)
+        {
+        	let currentRadio = get_checkedRadio()
+        	currentRadio.previousElementSibling.checked = true;
+        }
+        if(direction > 0)
+        {
+        	let currentRadio = get_checkedRadio()
+        	currentRadio.nextElementSibling.checked = true;
+        }
+        indicatorMoveAnim();
+      }
+      
+
+    });
+}
 
 
 
-	
-})
+
+// SLIDESHOW INDICATORS ANIMATION
+
+
+
